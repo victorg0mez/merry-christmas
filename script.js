47eb0766b96e0bdc1c773cfbf3c0d56429d0ae25
Victor Gomez <victorgomezchuquiej@gmail.com>
2024-12-11 00:47:35 -0600

    cambios sencillos

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ setTimeout(() => {
 }, 100); // retrase para cargar los cambios
 
 const audio = new Audio('../sound/We Wish You A Merry Christmas.mp3');
-document.getElementById('play-audio').addEventListener('touchstart', () => {
+document.getElementById('play-audio').addEventListener('click', () => {
   audio.play();
 });
 window.onload = () => {
